e036ee6f32cb0171a7371c93c58d8ba8701f8f7f
Andrew Sutton <andrewsydsutton@gmail.com>
2022-02-28 22:04:02 -0600

    featured song tweaking

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -30,16 +30,16 @@ const HomeComponent = () => {
                             <h2>Featured</h2>
                             <Divider style={{margin: "-.5rem 0 2rem"}}/>
                             <div className="ui stackable two column grid iframe-container">
+
                                 <iframe 
-                                    style={{maxWidth: "30rem", borderRadius: "12px"}}
-                                    src="https://open.spotify.com/embed/track/5vQn0HXFJ6EHt3mWLTcUam?utm_source=generator" 
-                                    width="100%" 
-                                    title="Fishbowl"
-                                    height="380" 
-                                    frameBorder="0" 
-                                    allowFullScreen="" 
-                                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                                />
+                                    allow="autoplay *; encrypted-media *; fullscreen *" 
+                                    frameborder="0" 
+                                    height="150" 
+                                    style={{width:"100%", maxWidth: "660px", overflow: "hidden", background: "transparent"}}
+                                    sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
+                                    src="https://embed.music.apple.com/us/album/fishbowl/1567786639?i=1567786640">
+                                    
+                                    </iframe>
                             </div>
                         </Grid.Column>
 
