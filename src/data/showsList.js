1725e0113b22a56b1ca11d91b0ad3dfdc48e3bdf
Andrew Sutton <andrewsydsutton@gmail.com>
2022-04-18 10:57:35 -0500

    Update shows list

--- src/data/showsList.js
+++ src/data/showsList.js
@@ -8,16 +8,16 @@ export const showsList = [
     //     eventLink: "",
     //     address: "2020 N Oakley Ave, Chicago, IL 60647"
     // },
-    {
-        date: "4/10/22",
-        venue: "The Chop Shop",
-        time: "Doors @ 5PM",
-        cover: "$15",
-        ticketLink : "https://link.dice.fm/g3f8d2e0400c",
-        venueLink: "https://chopshopchi.com/",
-        eventLink: "https://www.facebook.com/events/330479492353926",
-        address: "2033 W. North Ave. Chicago, IL 60647"
-    },
+    // {
+    //     date: "4/10/22",
+    //     venue: "The Chop Shop",
+    //     time: "Doors @ 5PM",
+    //     cover: "$15",
+    //     ticketLink : "https://link.dice.fm/g3f8d2e0400c",
+    //     venueLink: "https://chopshopchi.com/",
+    //     eventLink: "https://www.facebook.com/events/330479492353926",
+    //     address: "2033 W. North Ave. Chicago, IL 60647"
+    // },
     {
         date: "4/22/22",
         venue: "Gallery Cabaret",
